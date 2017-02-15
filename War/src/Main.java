e890fb18831cbbe73c8a5f6506ed46878d0b55ee
Magal <magalking8@gmail.com>
2017-02-15 21:37:02 +0200

    Update Main.java
    changed spacing and some other formatting added comments to question code correctness

--- War/src/Main.java
+++ War/src/Main.java
@@ -12,10 +12,10 @@ public class Main {
 	public static void main (String[] args){
 		Scanner reader = new Scanner(System.in);
 		
-		System.out.println("enter player1 name: \n");
+		System.out.println("Enter player1 name: \n");
 		String player1 = reader.next();
 		
-		System.out.println("enter player2 name: \n");
+		System.out.println("Enter player2 name: \n");
 		String player2 = reader.next();
 		
 		reader.close();
@@ -23,34 +23,33 @@ public class Main {
 		int p1 = 0;
 		int p2 = 0;
 		
-		
-		
-		int b = 0;
-		for(int i = 0; i<20; i++){
-			if (IsWin(player1Deck.pop(), player2Deck.pop()) == 1) 
+		int b = 0; // what does b do?
+		for(int i = 0; i<20; i++) { // why 20 iterations?
+			if (IsWin(player1Deck.pop(), player2Deck.pop()) == 1) {
 				p1++;
-			if (IsWin(player1Deck.pop(), player2Deck.pop()) == 2) 
+            } else if (IsWin(player1Deck.pop(), player2Deck.pop()) == 2) {
 				p2++;
-			
-			if (IsWin(player1Deck.pop(), player2Deck.pop()) == 3)  System.out.println("War");
-				
-			
-			if (IsWin(player1Deck.pop(), player2Deck.pop()) == 0) System.out.println("problem");;
-
-			if (b == 1) p1++;
+            } else if (IsWin(player1Deck.pop(), player2Deck.pop()) == 3) {
+                System.out.println("War");
+            } else if (IsWin(player1Deck.pop(), player2Deck.pop()) == 0) {
+                System.out.println("problem");
+            }
+            // b is never changed. perhaps b is the result of War?
+			if (b == 1) p1++; 
 			if(b == 2) p2++;
-			
-			
-		
+        }
 		if (p1 > p2) System.out.println(player1);
 		if (p2 > p1) System.out.println(player2);
-		}}
+		
+        //War doesn't work, at all
+    }
 	
 	// true -> p1 false -> p2
 	private static int War() {
 		player1Deck.pop();
 		player1Deck.pop();
 		player1Deck.pop();
+        // should be put in for loop for easier counting
 		player2Deck.pop();
 		player2Deck.pop();
 		player2Deck.pop();
@@ -63,11 +62,9 @@ public class Main {
 	private static int IsWin(card pop, card pop2) {
 		System.out.println("player 1 put: " + pop.getNumber() + " player 2 put: " + pop2.getNumber());
 		if (pop.getNumber()>pop2.getNumber()) return 1;
-		else 
-			if (pop.getNumber()<pop2.getNumber()) return 2;
-			
-			else if  (pop.getNumber()==pop2.getNumber()) return 3;
-			else return 0;
+		else if (pop.getNumber()<pop2.getNumber()) return 2;
+		else if  (pop.getNumber()==pop2.getNumber()) return 3;
+		else return 0;
 	}
 	
 	
